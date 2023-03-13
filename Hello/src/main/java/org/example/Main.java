f4137b16f30911a72295be2071fac562aa76e6af
Eugenyy Malgin <eugenimalgin@yandex.ru>
2023-03-13 20:15:51 +0300

    create Kanal

--- Hello/src/main/java/org/example/Main.java
+++ Hello/src/main/java/org/example/Main.java
@@ -1,7 +1,42 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] kanalbi = new String[7];
+        kanalbi[1] = "Первый канал";
+        kanalbi[2] = "Второй канал";
+        kanalbi[3] = "Третий канал";
+        kanalbi[4] = "Четвертый канал";
+        kanalbi[5] = "Пятый канал";
+        kanalbi[6] = "Шестой канал";
+        kanalbi[0] = "0";
+
+        System.out.println("Введите номер канала");
+
+        int canals;
+        do {
+            Scanner scanner = new Scanner(System.in);
+            canals = scanner.nextInt();
+            if (canals == 1) {
+                System.out.println(kanalbi[1]);
+            }
+            if (canals == 2) {
+                System.out.println(kanalbi[2]);
+            }
+            if (canals == 3) {
+                System.out.println(kanalbi[3]);
+            }
+            if (canals == 4) {
+                System.out.println(kanalbi[4]);
+            }
+            if (canals == 5) {
+                System.out.println(kanalbi[5]);
+            }
+            if (canals == 6) {
+                System.out.println(kanalbi[6]);
+            }
+        } while (canals >= 1 && canals <= 6);
     }
 }
