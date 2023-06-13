59ae34f1d792884baf10dd0fabd17b524f1c2062
Eugenyy Malgin <eugenimalgin@yandex.ru>
2023-06-13 20:57:06 +0300

    Add Running tests in parallel

--- DnsProject/src/test/java/cybersport/helper/WebDriverContainer.java
+++ DnsProject/src/test/java/cybersport/helper/WebDriverContainer.java
@@ -1,11 +1,18 @@
 package cybersport.helper;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class WebDriverContainer {
 
@@ -13,6 +20,9 @@ public class WebDriverContainer {
     public static WebDriver getDriver() {
         WebDriver driver;
         Browsers browsers = Browsers.valueOf(System.getProperty("browser", "chrome"));
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setPlatform(Platform.WINDOWS);
 
         switch (browsers) {
             case firefox -> driver = new FirefoxDriver();
@@ -25,7 +35,13 @@ public class WebDriverContainer {
                 ChromeOptions options = new ChromeOptions();
                 options.addArguments("--remote-allow-origins=*");
                 driver = new ChromeDriver(options);
+                caps.setBrowserName(Browser.CHROME.browserName());
             }
+        }
+        try {
+            driver = new RemoteWebDriver(new URL("http://192.168.1.102:4444/wd/hub"),caps);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
         }
         driver.manage().window().maximize();
 
